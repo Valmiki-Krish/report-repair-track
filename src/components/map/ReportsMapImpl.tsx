f2cd890158b482968b9b24528f806046299ca7e9
Krish Valmiki <kvalmiki40@gmail.com>
2026-09-19 12:44:16 +0530

    Move updated ReportsMapImpl into components map directory

--- src/components/map/ReportsMapImpl.tsx
+++ src/components/map/ReportsMapImpl.tsx
@@ -1,4 +1,5 @@
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { Link } from "@tanstack/react-router";
 import { STATUS_COLORS, isStatus, type Report } from "@/lib/reports";
