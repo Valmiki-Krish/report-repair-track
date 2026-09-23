d24f3c4ed9d625dcfd84d171bbcc96f6c7aaffb7
Krish Valmiki <kvalmiki40@gmail.com>
2026-09-23 20:02:37 +0530

    Remove reportLovableError from ErrorComponent
    Removed error reporting functionality from ErrorComponent.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,6 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
-import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Navbar } from "@/components/Navbar";
 
 function NotFoundComponent() {
@@ -39,8 +38,7 @@ function ErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
   console.error(error);
   const router = useRouter();
   useEffect(() => {
-    reportLovableError(error, { boundary: "tanstack_root_error_component" });
-  }, [error]);
+    
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-background px-4">
